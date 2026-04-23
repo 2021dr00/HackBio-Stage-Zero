9d92fa7c2ada96cd99be7bac427ad45d91a20536
Dharshana Rajkumar <92250650+2021dr00@users.noreply.github.com>
2026-04-24 00:46:48 +0530

    Add files via upload

---
+++ simple_code.py
@@ -0,0 +1,9 @@
+# User details
+name = "Dharshana Rajkumar"
+affiliation = "PSG College of Technology"
+gene = "BRCA1"
+organism = "Homo sapiens"
+
+# Output sentence
+print(f"Hi, my name is {name}, an alumini at the {affiliation}. "
+      f"My favorite gene is {gene} in {organism}.")
